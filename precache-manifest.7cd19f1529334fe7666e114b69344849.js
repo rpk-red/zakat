de0424e795c8c733087220583028faffae27b9a8
Goran Ribic <ribic.goran@hotmail.com>
2020-02-10 22:38:11 +0100

    Updates

---
+++ precache-manifest.7cd19f1529334fe7666e114b69344849.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82cf42188dc48814de02133bc36f1470",
+    "url": "/zakat/index.html"
+  },
+  {
+    "revision": "273244f58ec839c8f7ca",
+    "url": "/zakat/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "47ae735cd088bd581cb9",
+    "url": "/zakat/static/js/2.e42bddd1.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/zakat/static/js/2.e42bddd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273244f58ec839c8f7ca",
+    "url": "/zakat/static/js/main.1bc02090.chunk.js"
+  },
+  {
+    "revision": "2a1922b702431e7e229e",
+    "url": "/zakat/static/js/runtime-main.4b935a40.js"
+  },
+  {
+    "revision": "92ac99da30e00fb0e761c6d0a6c78e98",
+    "url": "/zakat/static/media/image.92ac99da.jpg"
+  }
+]);
