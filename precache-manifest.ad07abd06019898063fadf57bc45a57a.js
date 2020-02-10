1d6f62b4ac70812b3dd4cae75dbc72bcf2b6e99a
Goran Ribic <ribic.goran@hotmail.com>
2020-02-10 22:15:05 +0100

    Updates

---
+++ precache-manifest.ad07abd06019898063fadf57bc45a57a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1139b1d2fe2da287cc7c4b03bdb192f",
+    "url": "/zakat/index.html"
+  },
+  {
+    "revision": "3b6a4cd4c2ffb062680e",
+    "url": "/zakat/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "47ae735cd088bd581cb9",
+    "url": "/zakat/static/js/2.e42bddd1.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/zakat/static/js/2.e42bddd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b6a4cd4c2ffb062680e",
+    "url": "/zakat/static/js/main.5edb39a3.chunk.js"
+  },
+  {
+    "revision": "2a1922b702431e7e229e",
+    "url": "/zakat/static/js/runtime-main.4b935a40.js"
+  },
+  {
+    "revision": "92ac99da30e00fb0e761c6d0a6c78e98",
+    "url": "/zakat/static/media/image.92ac99da.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zakat/precache-manifest.1e258f9fa42c835ea45e628c4edc223b.js"
+  "/zakat/precache-manifest.ad07abd06019898063fadf57bc45a57a.js"
 );
 
 self.addEventListener('message', (event) => {
